7473309304725429c9d329e871291a439a9a2082
OlenkaZmiyuchka <103105921+OlenkaZmiyuchka@users.noreply.github.com>
2022-11-17 21:32:43 +0200

    update time in js

--- src/script.js
+++ src/script.js
@@ -7,7 +7,13 @@ function TimeNow() {
 
   let date = now.getDate();
   let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
   let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   let days = [
     "Sunday",
     "Monday",
